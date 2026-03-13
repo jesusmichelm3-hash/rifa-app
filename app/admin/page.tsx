16c1cf224a6935b3eaa220ff7144f0ed307630fb
Jesus Moreno <jesusmichelm3@gmail.com>
2026-03-13 16:54:07 -0700

    Forzando redeploy a Vercel

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -183,11 +183,13 @@ Sorteos501`;
     if (!logueado) {
 
         return (
-
-            <div className="min-h-screen flex items-center justify-center bg-gray-100 dark:bg-gray-900 transition">
-
-                <div className="bg-white dark:bg-gray-800 p-10 rounded-2xl shadow-xl w-full max-w-md">
-
+            <div
+                className="min-h-screen flex items-center justify-center bg-cover bg-center transition"
+                style={{
+                    backgroundImage: `url('logo.JPEG')`
+                }}
+            >
+                <div className="bg-white dark:bg-gray-800 bg-opacity-90 p-10 rounded-2xl shadow-xl w-full max-w-md">
                     <h1 className="text-3xl font-bold text-center mb-6 dark:text-white">
                         Panel Administrador
                     </h1>
@@ -212,13 +214,9 @@ Sorteos501`;
                     >
                         Iniciar sesión
                     </button>
-
                 </div>
-
             </div>
-
         );
-
     }
 
     return (
